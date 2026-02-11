d2ea1c7b52f9a34be518265ac1dec0d22c935d7c
kelicodes <kelidevske@gmail.com>
2026-02-11 18:52:03 +0300

    o

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './Context/ThemeContext.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
