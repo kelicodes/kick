da90b15cb0f5b8a45abe1593de3e211d6c097a50
unknown <you@example.com>
2026-04-04 12:02:49 +0300

    I

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,15 +1,21 @@
-import "./Card.css";
 import { Link } from "react-router-dom";
 
 const Card = ({ id, name, price, image }) => {
   return (
     <Link to={`/product/${id}`} className="card-link">
       <div className="card">
-        <img src={image} alt={name} />
+        
+        {/* IMAGE */}
+        <div className="card-img">
+          <img src={image} alt={name} />
+        </div>
+
+        {/* INFO */}
         <div className="card-info">
           <p className="card-name">{name}</p>
-          <p className="card-price">${price}</p>
+          <p className="card-price">ksh{price}</p>
         </div>
+
       </div>
     </Link>
   );
